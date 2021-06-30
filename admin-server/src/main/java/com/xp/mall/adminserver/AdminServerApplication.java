5cc924c1e5ef3c8e00f8f8e00a4da96e805058b8
xp <1329707185@qq.com>
2021-06-30 21:38:22 +0800

    services build

--- admin-server/src/main/java/com/xp/mall/adminserver/AdminServerApplication.java
+++ admin-server/src/main/java/com/xp/mall/adminserver/AdminServerApplication.java
@@ -3,7 +3,10 @@ package com.xp.mall.adminserver;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 @EnableAdminServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AdminServerApplication {
 
